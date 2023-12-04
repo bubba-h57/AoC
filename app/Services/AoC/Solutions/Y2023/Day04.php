3d30216aceda3e6e24c88bc3165e476fc0a1ded6
Bubba Hines <bubba@stechstudio.com>
2023-12-04 09:21:30 -0500

    Puzzles: Add some templates.

---
+++ app/Services/AoC/Solutions/Y2023/Day04.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Services\AoC\Solutions\Y2023;
+
+use App\Services\AoC\Interfaces\Day;
+use App\Services\AoC\Solutions\Solution;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+
+class Day04 extends Solution  implements Day
+{
+    public const day = 4;
+    public const year = 2023;
+    public const title = '';
+
+
+    public function __construct(?Collection $input = null)
+    {
+        parent::__construct($input);
+    }
+
+    public function part1()
+    {
+    }
+
+    public function part2()
+    {
+
+    }
+}
