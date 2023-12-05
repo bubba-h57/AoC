d5e336288c3e0ab426e70f5273a51cfac844ceae
Bubba Hines <bubba@stechstudio.com>
2023-12-05 11:04:30 -0500

    Puzzles 03: Part 1 Solution

--- app/Services/AoC/Solutions/Y2023/Day03.php
+++ app/Services/AoC/Solutions/Y2023/Day03.php
@@ -21,6 +21,60 @@ class Day03 extends Solution  implements Day
 
     public function part1()
     {
+        $total = 0;
+
+        $whereNumbers = [];
+
+        foreach ($this->input as $index => $line) {
+            preg_match_all('/\d+/', $line, $matches);
+            $matches = $matches[0];
+
+            $parsedLine = $line;
+
+            foreach ($matches as $match) {
+
+                $replaceString = str_repeat('?', strlen($match));
+                $position = strpos($parsedLine, $match);
+
+                $parsedLine = substr_replace($parsedLine, $replaceString, $position, strlen($match));
+                $whereNumbers[] = [$index, $position, $match];
+            }
+        }
+
+        foreach ($whereNumbers as $whereNumber) {
+            $line = $whereNumber[0];
+            $pos = $whereNumber[1];
+            $number = $whereNumber[2];
+            $length = strlen($number);
+
+            for ($i = -1; $i <= $length; $i++) {
+
+                $characters = [];
+
+                if ($line - 1 >= 0 && $pos + $i >= 0 && $pos + $i < strlen($this->input[0])) {
+                    $characters[] = $this->input[$line - 1][$pos + $i];
+                }
+
+                if ($pos + $i >= 0 && $pos + $i < strlen($this->input[0])) {
+                    $characters[] = $this->input[$line][$pos + $i];
+                }
+
+                if ($line + 1 < count($this->input) && $pos + $i >= 0 && $pos + $i < strlen($this->input[0])) {
+                    $characters[] = $this->input[$line + 1][$pos + $i];
+                }
+
+                $arrayContainsSymbol = array_filter($characters, function ($character) {
+                    return !is_numeric($character) && $character !== '.';
+                });
+
+                if (!empty($arrayContainsSymbol)) {
+                    $total += $number;
+                    break;
+                }
+            }
+        }
+
+        return $total;
     }
 
     public function part2()
